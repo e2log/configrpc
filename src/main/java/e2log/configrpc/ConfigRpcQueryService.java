fd0ef1bd55d00a2771cd51d46ec20fe81e21a3d8
Matt Hicks <matt.hicks@e2log.com>
2021-03-31 17:21:35 -0500

    Adding error handling to match presentation

--- src/main/java/e2log/configrpc/ConfigRpcQueryService.java
+++ src/main/java/e2log/configrpc/ConfigRpcQueryService.java
@@ -1,7 +1,8 @@
 package e2log.configrpc;
 
 
-import com.google.protobuf.Any;
+import io.grpc.Status;
+import io.grpc.StatusException;
 import io.grpc.stub.StreamObserver;
 import io.opentracing.contrib.grpc.TracingServerInterceptor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,18 +24,28 @@ public class ConfigRpcQueryService extends ConfigrpcServiceGrpc.ConfigrpcService
   @Override
   public void findString(FindStringRequest request, StreamObserver<FindStringResponse> responseObserver) {
     log.debug("findString({})", request);
-    val response = FindStringResponse.newBuilder();
     val key = request.getKey();
-    val respContent = KeyString.newBuilder().setKey(key);
-    
-    val maybeJsonString = request.hasJsonPath() ? configurationRepo.queryObjectByDocumentKeyAndJsonPath(key, request.getJsonPath().getValue()) :
-                                                   configurationRepo.fetchDocumentByKey(key);
-    
-    maybeJsonString.ifPresent(respContent::setValue);
-    response.setKv(respContent);
-    
-    responseObserver.onNext(response.build());
-    responseObserver.onCompleted();
+
+    try {
+      val maybeJsonString = request.hasJsonPath() ? configurationRepo.queryObjectByDocumentKeyAndJsonPath(key, request.getJsonPath().getValue()) :
+        configurationRepo.fetchDocumentByKey(key);
+
+      maybeJsonString.ifPresentOrElse(value -> {
+          // Build and return the protobuf response
+          val respContent = KeyString.newBuilder().setKey(key).setValue(value);
+          val response = FindStringResponse.newBuilder().setKv(respContent);
+          responseObserver.onNext(response.build());
+          responseObserver.onCompleted();
+        }, () ->
+          // Return exception because expected value was not found for provided key
+          responseObserver.onError(
+            new StatusException(Status.NOT_FOUND.withDescription("Cannot find value with key=" + key))
+          )
+      );
+    } catch(IllegalArgumentException e) {
+      // Wrapping IAE exception and returning appropriate gRpc status via onError
+      responseObserver.onError(new StatusException(Status.INVALID_ARGUMENT.withDescription(e.getMessage())));
+    }
   }
   
   @Override
